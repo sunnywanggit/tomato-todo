9d084c1527cd7e74fbacc4947bac886e9e50dd76
ergouwang <mrergouwang@gmail.com>
2020-02-23 14:47:34 +0800

    完善home页面数据操作

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,4 @@
-//引入id生成器
-let {idGenerator} = require('../../utils/idGenerator.js')
+const {http} = require('../../utils/http.js')
 
 Page({
 
@@ -8,94 +7,49 @@ Page({
      */
     data: {
         visible: false,
-        lists: [
-            {id: 1, text: 'do', finished:false},
-            {id: 2, text: 'what', finished:false},
-            {id: 3, text: 'tomorrow', finished:false},
-            {id: 4, text: 'do what tomorrow', finished:false}
-        ]
-
+        lists: []
     },
+    onShow(){
+        http.get('/todos?completed=false').then(respsonse=>{
+            let unCompleted = respsonse.data.resources
+            this.setData({lists:unCompleted})
+        })
+    },
+    // 确认任务创建
     confirm(event) {
-        let obj = {}
-        obj.id = this.data.lists.length + 1
-        obj.text = event.detail;
-        obj.finished = false
-        let newLists = this.data.lists
-        if (event.detail) {
-            newLists.push(obj)
-            this.setData({lists: newLists})
-            this.setData({visible: false})
+        console.log('confirm');
+        let content = event.detail;
+        if (content) {
+            http.post(
+                '/todos',
+                { description: content }
+            ).then(response => {
+                let todo = response.data.resource;
+                let newArr = []
+                newArr.push(todo)
+                this.setData({lists:newArr})
+                this.setData({visible:false})
+            })
         }
     },
     // 取消点击confirm框
     cancel(event) {
         this.setData({visible: false})
     },
+    //创建一个任务
     createTask() {
         this.setData({visible: true})
+
     },
-    finishTask(event){
+    finishTask(event) {
         let index = event.currentTarget.dataset.index;
-        this.data.lists[index].finished = true;
-        this.setData({lists:this.data.lists})
+        this.data.lists[index].completed = true;
+        this.setData({lists: this.data.lists})
     },
     //点击开始闹钟
-    startTiming(){
+    startTiming() {
         wx.navigateTo({
             url: '/pages/tomato/tomato'
         })
     },
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
