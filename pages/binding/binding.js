18bfa6018934727408779c4ed95e3c060e224047
ergouwang <mrergouwang@gmail.com>
2020-02-24 21:32:13 +0800

    基本完成小程序开发"

--- pages/binding/binding.js
+++ pages/binding/binding.js
@@ -1,80 +1,33 @@
+let {http}= require('../../utils/http.js')
+
 Page({
     data: {
         accountValue: '',
         passwordValue: '',
-        onOff:true
     },
     inputAccount: function (event) {
         let value = event.detail.value
-        this.setData({accountValue:value})
-        console.log(this.data.accountValue);
+        this.setData({accountValue: value})
     },
-    passwordInput:function(event){
+    passwordInput: function (event) {
         let value = event.detail.value
-        this.setData({passwordValue:value})
+        this.setData({passwordValue: value})
+
+    },
+    onRegister: function () {
+        console.log(this.data.accountValue);
         console.log(this.data.passwordValue);
+        http.post('/bindings',{
+            account: this.data.accountValue,
+            password_digest: this.data.passwordValue
+        }).then(response=>{
+            console.log(response);
+            wx.setStorageSync('my',response.data.resource)
+            wx.redirectTo({
+                url: '/pages/home/home'
+            })
+        })
+
 
     },
-    register:function(){
-
-        if(this.data.onOff){
-            this.setData({onOff:false})
-        }else{
-            this.setData({onOff:true})
-        }
-
-
-    },
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
